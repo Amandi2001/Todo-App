d85e7806c62e423e7f2ee9c269790c0f66766d14
Amandi2001 <168396897+Amandi2001@users.noreply.github.com>
2024-06-10 17:07:54 +0300

    final commit

---
+++ app/src/main/java/com/example/todoapp/Dev_info.java
@@ -0,0 +1,14 @@
+package com.example.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dev_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dev_info);
+    }
+}
